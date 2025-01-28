dea4185261b12530d73627e2b57116ab3399238f
= <=>
2025-01-28 13:21:32 +0100

    Added json accept middleware for api path ONLY

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->prependToGroup('api', \App\Http\Middleware\AlwaysAcceptJson::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
