e91448ac252d576781c7a658960ec197d6e9ff02
= <=>
2025-01-28 13:28:03 +0100

    Added MethodNotAllowed Exception to the bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -21,4 +22,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->renderable(function (NotFoundHttpException $e) {
             return response()->json(['message' => 'Object not found'], 404);
         });
+
+        // Add for MethodNotAllowedHttpException here
+        $exceptions->renderable(function (MethodNotAllowedHttpException $e) {
+            return response()->json(['message' => 'Method not allowed'], 405);
+        });
+
     })->create();
