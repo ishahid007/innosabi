5afb99d2de2f99fa200f0fcd75d4c17ff3d233b4
= <=>
2025-01-30 22:39:17 +0100

    Added RateLimit for public api

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,7 @@
 use App\Http\Controllers\Api\SuggestionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('suggestion', SuggestionController::class)->name(name: 'suggestion.search');
+//
+Route::group(['middleware' => 'throttle:api'], function () {
+    Route::get('/suggestion', SuggestionController::class)->name('suggestion.search');
+});
